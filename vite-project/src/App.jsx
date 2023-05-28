b25b15b4adcb7402570c69275f29f675870096a2
Denio Nhanale <dnhanale@gmail.com>
2023-05-28 12:21:41 +0200

    Setup

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,12 +1,21 @@
+import { useState } from "react"
 import "./styles.css"
 
 export default function App() {
+  const [newItem, setNewItem] = useState("")
+
+  function handleSubmit(e) {
+    e.preventDefault()
+  }
+
   return (
   <>
-  <form className="new-item-form">
+  <form onSubmit={handleSubmit} className="new-item-form">
    <div className="form-row">
     <label htmlFor="item">New Item</label>
-    <input type="text" id="item" />
+    <input value={newItem} 
+    onChange={e => setNewItem(e.target.value)}
+    type="text" id="item" />
    </div>
    <button className="btn">Add</button>
   </form>
